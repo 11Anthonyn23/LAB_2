22b5402fc747586e65001274f671e26ca33209f6
11Anthonyn23 <anthonyn23@mail.fresnostate.edu>
2026-03-24 05:16:50 -0700

    Add files via upload

---
+++ edited_files/test_lib.svh
@@ -0,0 +1,12 @@
+// This file is part of CSCE-416
+// Texas A&M University 2025
+// File name: test/test_lib.sv
+
+`include "base_test.sv"
+`include "HACK_ADD.sv"
+`include "HACK_MULT.sv"
+`include "HACK_DIV.sv"
+`include "HACK_MAX.sv"
+`include "HACK_MAX2.sv"
+`include "HACK_RECT.sv"
+//LAB7: TO-DO: Add the HACK_DIV and HACK_MULT tests, both in this file and by creating the associated 2 .sv files
